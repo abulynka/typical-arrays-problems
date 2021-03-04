b76c76cbffc97187064fe3b98872e4c04b2327a4
Aliaksandr Bulynka <abulynka@gmail.com>
2021-03-05 00:57:30 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-
+/**
+ * Returns min value in array
+ *
+ * @param array
+ * @returns {number}
+ */
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.isArray === false || array.length === 0) {
+        return 0
+    }
+
+    return array.reduce(
+        (last, current) => {
+            if (last > current) {
+                return current;
+            }
+            return last;
+        }
+    );
 }
 
+/**
+ * Returns max value in array
+ *
+ * @param array
+ * @returns {number}
+ */
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.isArray === false || array.length === 0) {
+        return 0
+    }
+
+    return array.reduce(
+        (last, current) => {
+            if (last > current) {
+                return last;
+            }
+            return current;
+        }
+    );
 }
 
+/**
+ * Returns avg value in array
+ *
+ * @param array
+ * @returns {number}
+ */
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.isArray === false || array.length === 0) {
+        return 0
+    }
+
+    return array.reduce(
+        (last, current) => {
+            return last + current;
+        }
+    ) / array.length;
 }
